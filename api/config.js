4b210ea17266230d7c94be033cdbfb31cbce4c32
rsmediazero <107334806+rsmediazero@users.noreply.github.com>
2025-10-17 12:38:33 +0000

    g

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 // config.js
 export const tokenConfig = {
-    token: "your-token-here",
-    deviceId: "your-device-id-here"
+    token: "Bearer ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnlaV2RwYzNSbGNsUjVjR1VpT2lKVVJVMVFJaXdpZFhObGNrbGtJam96TURnME1EQXhPVEY5LllzSkNzeTh5YkE3c19qOTNad0hHYmJrSFgzMkQ4V3psNXRPX0oydEpxSE0=",
+    deviceId: "de20ff3f-350c-49fa-979a-bfa623b389bf"
 };
